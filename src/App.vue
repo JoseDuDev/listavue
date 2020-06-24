c03b9d148bf44df5a817f56626612508c527aefd
JoseDuDev <josedudev@gmail.com>
2020-06-24 14:58:50 -0300

    Header bootstrap vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <HeaderPage />
+    </header>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
+
+<script>
+import HeaderPage from "./views/parts/HeaderPage.vue";
+
+export default {
+  name: "App",
+  computed: {},
+  components: {
+    HeaderPage
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ src/views/parts/Rodape.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+    © 2020 Copyright:
+    <a href="#"> Teste.com</a>
+  </div>
+  <!-- Copyright -->
+</template>
+
+<script>
+export default {
+  name: "Rodape"
+};
+</script>
+
+<style scoped></style>
